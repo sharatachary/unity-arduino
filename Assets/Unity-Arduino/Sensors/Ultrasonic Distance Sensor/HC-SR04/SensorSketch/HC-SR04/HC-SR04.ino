64276755a2738408de85fe5ad13e82d85e3916e7
sharat-xrdev <sharat.xrdev@gmail.com>
2026-01-29 00:09:10 +0530

    Added sensor sketch file.

---
+++ Assets/Unity-Arduino/Sensors/Ultrasonic Distance Sensor/HC-SR04/SensorSketch/HC-SR04/HC-SR04.ino
@@ -0,0 +1,29 @@
+
+const int trigPin = 10;
+const int echoPin = 13;
+const float MS_TO_CMS = 1.0 / 29 / 2;
+
+long cm;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+}
+
+void loop() {
+  
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  cm = pulseIn(echoPin, HIGH) * MS_TO_CMS;
+  
+  Serial.print(cm);
+  Serial.println();
+
+  delay(500);
+}
